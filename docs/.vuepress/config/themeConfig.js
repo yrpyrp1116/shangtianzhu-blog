2baacd2d955732ad692ae163e8fec1280124ab7b
尚天竹 <1206573971@qq.com>
2021-10-18 16:42:58 +0800

    my blog

---
+++ docs/.vuepress/config/themeConfig.js
@@ -0,0 +1,48 @@
+const nav = require('./nav')
+
+module.exports = {
+  nav,
+  sidebarDepth: 2,
+  lastUpdated: '上次更新',
+  searchMaxSuggestions: 10,
+  // repo: 'yrpyrp1116/au-blog',
+  // docsDir: 'docs',
+  // editLinks: true,
+  // editLinkText: '编辑',
+  sidebar: {
+    mode: 'structuring',
+    collapsable: false
+  },
+  rightMenuBar: true,
+  pageButton: true,
+  // bodyBgImg: (process.env.NODE_ENV === 'development' ? '' : '/blog') + '/images/bg-6.jpeg',
+  // bodyBgImgOpacity: 0.8,
+  footer: {
+    createYear: 2021,
+    copyrightInfo: 'coderly | <a href="https://github.com/yrpyrp1116" target="_blank">MIT License</a>'
+  },
+  blogger: {
+    avatar: 'https://avatar-static.segmentfault.com/249/884/2498845138-602fc45007b7c_big64',
+    name: '尚天竹',
+    slogan: '总有人要赢，为什么不能是我',
+  },
+  author: {
+    name: '尚天竹',
+    link: 'https://github.com/yrpyrp1116',
+  },
+  social: {
+    iconfontCssFile: '//at.alicdn.com/t/font_2382143_eetu51zaufa.css',
+    icons: [
+      {
+        iconClass: 'iconshejiaotubiao-46',
+        title: '知乎',
+        link: 'https://www.zhihu.com/people/yrp-48/posts',
+      },
+      {
+        iconClass: 'iconGitHub',
+        title: 'GitHub',
+        link: 'https://github.com/yrpyrp1116',
+      },
+    ],
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,22 @@
+---
+
+home: true
+heroImage: /images/au.png # 显示在主页的图片
+tagline: 不积跬步无以至千里 # 显示在主页的标题
+
+features: # 显示在主页的分类
+
+- title: 前端总结
+  details: JavaScript、ES6、Vue 框架以及 html(5)/css(3)，前端页面相关技术
+  link: /web/
+  imgUrl: /images/web.png # 图片以 docs/.vuepress/public 为根节点
+- title: 学习笔记
+  details: Vue、axios、插件等源码的理解
+  link: /note/
+  imgUrl: /images/note.png
+- title: 项目积累
+  details: 开源的项目以及项目中遇到的问题及其解决方法
+  link: /project/
+  imgUrl: /images/project.png
+
+footer: MIT Licensed | Copyright ? 2021-present coderly
